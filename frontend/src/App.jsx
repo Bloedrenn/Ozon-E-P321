8b740621e6792ec3f7ea5d37a94157faed0014fb
Bloedrenn <bloedrenn@gmail.com>
2025-12-30 19:56:58 +0300

    feat(items-list): Вывел список вещей в App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,45 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL
+
 function App() {
-  return null
+  // items = [
+  //   {id: '1', name: 'Xiaomi Redmi 10', description: 'Топ за свои деньги', isAvailable: true},
+  //   {id: '2', name: 'Iphone X', description: 'Крутой телефон (нет) и дорогой (да)', isAvailable: false}
+  // ]
+  const [items, setItems] = useState([])
+  const [itemsLoading, setItemsLoading] = useState(true)
+  const [itemsError, setItemsError] = useState(null)
+
+  useEffect(() => {
+    axios.get(`${API_URL}/items`)
+      .then(response => {
+        setItems(response.data)
+        setItemsLoading(false)
+      })
+      .catch(error => {
+        console.error(error)
+        setItemsLoading(false)
+        setItemsError(`Ошибка загрузки вещей: ${error}`)
+      })
+  }, [])
+
+  if (itemsLoading) return <div>Загрузка...</div>
+  if (itemsError) return <div>{itemsError}</div>
+
+  return (
+    <>
+      <h1>Список вещей</h1>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            <strong>{item.name}</strong>: {item.description}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default App
